c9040ac80ce29c82121a7863a43394127dec7a30
KimHaechan <ckssmsla98@naver.com>
2023-01-13 10:45:12 +0900

    api controller start

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 2. HELP.md -> README.md로 이름변경, gitignore 에서 해당 파일 제거
 3. application.properties -> application.yml로 이름 변경
 4. yml 설정으로 톰캣 포트 변경하기
+
+
 ```
 server:
   port: 8181
@@ -10,3 +12,12 @@ server:
 5. devTools 설정
 - 파일 -> 설정 -> 빌드,실행, 배포 -> 컴파일러 -> 프로젝트 자동빌드 체크
 - 파일 -> 설정 -> 고급설정 -> 컴파일러 -> 개발된 애플리케이션 ~~~ auto-make 체크
+
+
+## 로그 레벨(중요도) 정리
+1. trace : 제일 안중요함, 코드를 추적할때, 기능적 부분을 찾을때 사용
+2. debug : 말그대로 디버그할때, 도움이되는 정보
+3. info : 표준 로그 레벨, 서비스 시작이나 종료
+4. warm : 에러는 아닌데 좀 수상한 정보를 찍는 경고
+5. error : 애플리케이션이 작동하지 않는 수준의 정보( exception etc...)
+6. fatal : 심각한 오류 장애수준
---
+++ src/main/java/com/example/apibasic/req/ApiContoller1.java
@@ -0,0 +1,48 @@
+package com.example.apibasic.req;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Controller // 클라이언트가 보낸 요청을 받을 수 있다리
+@Slf4j // 로그 라이브러리
+@ResponseBody    @RequestMapping("/say")
+public class ApiContoller1 {
+
+    // 요청 처리 메소드
+    // request 매핑을 그냥 쓰면 allow 제한 없이 모든 메소드를 수용하게 된다.
+    @RequestMapping(value = "/say/hello", method = {GET, POST})
+    public String hello(HttpServletRequest request) {
+
+        // System 접근하면 안되는 이유 native 하드웨어 언어(C와 같은)를 사용하기 때문에 리소스 낭비 심함 추가적 파일 출력 소스코드도 필요함
+        // 어쨋든 리소스도 많이 낭비하고 실행될 테스크가 복잡하다잉
+        log.info("/hello spring - {}", request.getMethod());
+        log.trace("trace");
+        log.debug("debug");
+        log.warn("warn");
+        log.error("errrrr");
+        return " ";
+    }
+
+    @GetMapping("/greet")
+    public String greet(){
+        log.info("/say/greet GET 요청");
+        return "";
+    }
+    @PostMapping("/greet")
+    public String greet2(){
+        log.info("/say/greet Post 요청");
+        return "";
+    }
+
+
+
+}
